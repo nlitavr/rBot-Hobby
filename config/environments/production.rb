3acad0035aca8f25d5cd0b8e080d807dda4300a4
nlitavr <nlitavr@loaddynamix.com>
2014-08-18 11:57:07 -0700

    added User login / registration UI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #placeholder for heroku
+  config.action_mailer.default_url_options = { :host => '' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
